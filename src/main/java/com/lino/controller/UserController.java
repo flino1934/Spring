4c01cea244dee63b94437a4813f11b179459076d
flino1934 <f.lino1934@hotmail.com>
2022-02-24 19:55:58 -0300

    Delete User
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/controller/UserController.java
+++ src/main/java/com/lino/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,15 @@ public class UserController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 
 		return ResponseEntity.created(uri).body(obj);
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+		userService.delete(id);
+
+		return ResponseEntity.noContent().build();
 
 	}
 
--- src/main/java/com/lino/service/UserService.java
+++ src/main/java/com/lino/service/UserService.java
@@ -27,9 +27,15 @@ public class UserService {
 	}
 
 	public User insert(User obj) {
-		
+
 		return repository.save(obj);
-		
+
 	}
-	
+
+	public void delete(Long id) {
+
+		repository.deleteById(id);
+
+	}
+
 }
